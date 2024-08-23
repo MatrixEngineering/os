72e290e7fc711e2f5ca7ff34e31a782ffd43bb6f
senmeannrealwat <meann.sen@realwat.net>
2024-08-23 12:30:34 +0700

    Implement writer in VGA text mode

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,7 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        let mut color = 0xc;
-        if i % 2 == 0 {
-            color = 0x9;
-        }
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = color;
-        }
-    }
-
+    vga_buffer::print_something();
     loop {}
 }
 
